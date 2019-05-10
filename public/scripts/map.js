d440bfa519927ea56920c0b812566a80ea393be1
Jess Ting <jessting@Jesss-MacBook-Air-2.local>
2019-05-10 00:57:22 -0400

    Andrew 5/10

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -6,3 +6,17 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: nycCords});
     var marker = new google.maps.Marker({position: markerCords, map: map});
   }
+
+  //function that inputs an array of coordinates and outputs map with markerCords
+  exports.putMarkers = function(array, address) {
+    var mapA;
+    var center=address; //address = {lat, lng};
+    var zoomedIn=15; //experimental value
+    var markerArray = {test: "useless"};
+    function initMap() {
+      mapA = new google.maps.Map(document.getElementById('map'), {zoom: zoomedIn, center: address});
+      for(var i=0; i<array.length; i++) {
+        markerArray["marker"+(i+1)]=new google.maps.Marker({position: array[i], map: mapA});
+      }
+    }
+  };
