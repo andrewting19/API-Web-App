3b805dacd4af52878449d4eda54cecaa4b371b31
sadigulcelik <sadigulcelik@gmail.com>
2019-05-10 19:03:40 -0400

    asdferwh

--- s.js
+++ s.js
@@ -65,7 +65,6 @@ app.get('/', function (request, response) {
 //         title: "Main"
 //     });
 // });
-
 app.get('/about', function (request, response) {
     console.log("Get request: /about");
 
